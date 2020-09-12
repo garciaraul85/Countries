68a5928bcf2224dcfa7f93d30b1dec5120deed18
raulgarcia <garcia.raul@gmail.com>
2020-09-12 09:45:58 -0400

    Pagination added

--- Countries/Countries/CountriesTableTableViewController.swift
+++ Countries/Countries/CountriesTableTableViewController.swift
@@ -24,7 +24,7 @@ class CountriesTableTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.tableView.isEditing = true
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
